413aaf9e765b1e8cd232fa2b61d50579ceb58ae2
Matthew Phillips <phillips.matthew829@gmail.com>
2022-07-15 14:38:21 -0500

    Add relationships

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -1,3 +1,6 @@
 class HotelSerializer < ActiveModel::Serializer
   attributes :id, :name, :company, :city, :country
+
+  has_many :bookings
+    has_many :users, through: :bookings
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest
+
+  has_many :bookings
+    has_many :hotels, through: :bookings
 end
