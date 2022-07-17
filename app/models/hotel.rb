d4ca3429e37843538cc469fc30cb123005b2489f
Matthew Phillips <phillips.matthew829@gmail.com>
2022-07-17 11:07:14 -0500

    Add Hotel validations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,9 @@
 class Hotel < ApplicationRecord
     has_many :bookings
     has_many :users, through: :bookings
+
+    validates :name, precense: true, uniqueness: true
+    validates :company, precense: true
+    validates :city, precense: true
+    validates :country, precense: true
 end
