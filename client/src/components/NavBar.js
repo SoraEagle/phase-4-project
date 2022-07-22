0223fd5f5f6356d06663f4e50038ee20efa73c53
Matthew Phillips <phillips.matthew829@gmail.com>
2022-07-22 13:39:10 -0500

    Create NavBar.js

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from 'react-dom'
+
+const linkStyles = {};
+
+function NavBar(){
+    return(
+        <div id="nav">
+            <NavLink>
+                {/*  */}
+            </NavLink>
+            <NavLink>
+                {/*  */}
+            </NavLink>
+        </div>
+    );
+}
+
+export default NavBar;
