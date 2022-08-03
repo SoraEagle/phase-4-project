f3ea1c62babf3de73c15b5038af41342143a6aeb
Matthew Phillips <phillips.matthew829@gmail.com>
2022-08-03 14:36:29 -0500

    Edit wrapping

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,28 +2,28 @@ import React, {useState} from "react";
 
 function LoginForm({onLogin}){
     const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  function handleSubmit(e){
-    e.preventDefault();
-    setIsLoading(true);
-    fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    }).then((r) => {
-      setIsLoading(false);
-      if (r.ok){
-        r.json().then((user) => onLogin(user));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });
-  }
+    function handleSubmit(e){
+        e.preventDefault();
+        setIsLoading(true);
+        fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }).then((r) => {
+            setIsLoading(false);
+            if (r.ok){
+                r.json().then((user) => onLogin(user));
+            } else{
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
+    }
 
     return(
         <form onSubmit={handleSubmit}>
