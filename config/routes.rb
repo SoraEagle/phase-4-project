c392af0fc2a04e54f0426901fa061098f4e5a657
Matthew Phillips <phillips.matthew829@gmail.com>
2022-08-22 14:12:07 -0500

    Remove namespace api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
     resources :hotels, only: [:index, :show]
-    resources :users, only: [:create, :show]
+    resources :users
     get 'sessions/create'
     get 'sessions/destroy'
     post "/login", to: "sessions#create"
@@ -10,7 +9,6 @@ Rails.application.routes.draw do
     get "/me", to: "users#show"
     # Routing logic: fallback requests for React Router.
     # Leave this here to help deploy your app later!
-  end
   
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
