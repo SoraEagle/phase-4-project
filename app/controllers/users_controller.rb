e7d498769a07548e460a1512c28599ebef4d665b
Matthew Phillips <phillips.matthew829@gmail.com>
2022-08-24 15:54:12 -0500

    Comment out unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,52 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-    skip_before_action :authorized, only: :create
-    before_action :set_user, only: [:show, :update, :destroy]
+    skip_before_action :authorize, only: :create
+    # before_action :set_user, only: [:show, :update, :destroy]
 
-    def index
-        @users = User.all
-        render json: @users
-    end
+    # def index
+    #     @users = User.all
+    #     render json: @users
+    # end
 
     def show
-        render json: @user
+        render json: @current_user
     end
 
     def create
-        @user = User.new(user_params)
-
-        if @user.save
-            @token = encode_token({user_id: @user.id})
-            render json: {user: @user, token: @token}, status: :created
-        end
-        # session[:user_id] = user.id
-        # render json: user, status: :created
+        byebug
+        @user = User.create!(user_params)
+        # if @user.save
+            # @token = encode_token({user_id: @user.id})
+        session[:user_id] = @user.id
+        render json: {user: @user}, status: :created
+        console.log("User")
+        # end
     end
 
-    def update
-        if @user.update(user_params)
-            render json: @user
-        else render json: @user.errors, status: :unprocessable_entity
-    end
+    # def update
+    #     if @user.update(user_params)
+    #         render json: @user
+    #     else render json: @user.errors, status: :unprocessable_entity
+    #     end
+    # end
 
-    def destroy
-        @user.destroy
-    end
+    # def destroy
+    #     @user.destroy
+    # end
 
-    def get_current_user
-        if logged_in?
-            render json: current_user, status: :ok
-        else
-            render json: {message: ["Not Logged In"]}, status: :ok
-        end
-    end
+    # def get_current_user
+    #     if logged_in?
+    #         render json: current_user, status: :ok
+    #     else
+    #         render json: {message: ["Not Logged In"]}, status: :ok
+    #     end
+    # end
 
     private
-    def set_user
-        @user = User.find(params[:id])
-    end
+    # def set_user
+    #     @user = User.find(params[:id])
+    # end
 
     def user_params
-        params.require(:user).permit(:username, :password, :password_confirmation)
+        params.permit(:username, :password)
     end
 end
