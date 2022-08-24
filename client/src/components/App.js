29f997b266e7a141c8268b4500e3870d4786ff01
Matthew Phillips <phillips.matthew829@gmail.com>
2022-08-24 11:49:10 -0500

    Edit file structure and inherited methods

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,51 +1,45 @@
 import React, {useEffect, useState} from "react"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// import logo from './logo.svg';
 import './App.css';
-// import Login from "../pages/Login";
 import Footer from "./Footer";
-import NavBar from "./NavBar";
+import NavBar from "./Navigation/NavBar";
 import Home from "../pages/Home";
 import Hotels from "../pages/Hotels";
-import LoginForm from "./LoginForm";
-import SignUpForm from "./SignUpForm";
+import LoginForm from "./Authentication/LoginForm";
+import SignUpForm from "./Authentication/SignUpForm";
 
 function App(){
-  const [user, setUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
   
   const loginUser = user => {
-    if(user){
-      setUser(user);
+      setCurrentUser(user);
       setLoggedIn(true);
-    }else{
-      setUser(null);
-      setLoggedIn(false);
-    }
   }
 
   const logOutUser = () => {
-    setUser({});
+    setCurrentUser({});
     setLoggedIn(false);
   }
 
-  useEffect(() => {
-    // auto-login
-    fetch("/api/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   // auto-login
+  //   fetch("/api/me").then((r) => {
+  //     if (r.ok) {
+  //       r.json().then((currentUser) => setCurrentUser(currentUser));
+  //     }
+  //   });
+  // }, []);
   
   return(
       <Router>
+        {loggedIn ? <h1>Logged In!</h1> : null}
         <NavBar loggedIn={loggedIn} setLoggedIn={setLoggedIn} logOutUser={logOutUser} />
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/hotels"} element={<Hotels loggedIn={loggedIn} setLoggedIn={setLoggedIn} loginUser={loginUser} />} />
-          <Route path={"/login"} element={<LoginForm user={user} onLogin={setUser} />} />
-          <Route path={"/signup"} element={<SignUpForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} loginUser={loginUser} />} />
+          <Route path={"/login"} element={<LoginForm currentUser={currentUser} onLogin={setCurrentUser} />} />
+          <Route path={"/signup"} element={<SignUpForm loginUser={loginUser} />} />
         </Routes>
         <Footer />
       </Router>
