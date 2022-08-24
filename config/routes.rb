cd6110638999ce4a25c683aceb382cb91ea3ec69
Matthew Phillips <phillips.matthew829@gmail.com>
2022-08-24 11:35:28 -0500

    Remove uneeded code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-    resources :hotels, only: [:index, :show]
+    resources :hotels, only: [:index, :create, :show]
     resources :users
-    get 'sessions/create'
-    get 'sessions/destroy'
     post "/login", to: "sessions#create"
     delete "/logout", to: 'sessions#destroy'
     post "/signup", to: "users#create"
