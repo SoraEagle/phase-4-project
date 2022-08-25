607eeb1e8b833ba6bf20631eb70595438cb19b87
Matthew Phillips <phillips.matthew829@gmail.com>
2022-08-25 11:57:34 -0500

    Fix typos

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,8 +2,8 @@ class Hotel < ApplicationRecord
     has_many :bookings
     has_many :users, through: :bookings
 
-    # validates :name, precense: true, uniqueness: true
-    # validates :company, precense: true
-    # validates :city, precense: true
-    # validates :country, precense: true
+    # validates :name, presence: true, uniqueness: true
+    # validates :company, presence: true
+    # validates :city, presence: true
+    # validates :country, presence: true
 end
