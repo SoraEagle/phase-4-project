dbc2fcf5eaae55e8beca0ed01a149586dbb24921
Matthew Phillips <phillips.matthew829@gmail.com>
2022-09-13 13:40:45 -0500

    Move hotel.jsx to hotels folder

--- client/src/components/Hotels/Hotel.jsx
+++ client/src/components/Hotels/Hotel.jsx
@@ -1,11 +1,12 @@
 import React, {useContext, useState} from 'react';
-import {HotelsContext} from "../context/hotelsList";
+import {HotelsContext} from "../../context/hotelsList";
 
-function Hotel({hotel}){
+function Hotel({currentUser, hotel}){
   const {hotels, setHotels} = useContext(HotelsContext);
   const [booked, setBooked] = useState(false);
   
   function deleteHotel(){
+    // debugger
     console.log("Deleting Hotel...");
     fetch(`http://localhost:3001/hotels/${hotel.id}`, { // DELETE fetch request.
     method: "DELETE",
@@ -15,7 +16,8 @@ function Hotel({hotel}){
   }
 
   function onDeleteHotel(deletedHotel){
-    const updatedHotels = hotels.filter((hotel) => hotel.id!== deletedHotel.id);
+    // debugger
+    const updatedHotels = hotels.filter((hotel) => hotel.id !== deletedHotel.id);
     setHotels(updatedHotels);
     console.log("Hotel.jsx: Hotel has been deleted");
   }
@@ -23,21 +25,20 @@ function Hotel({hotel}){
   function toggleBooking(){
     setBooked(!booked);
     console.log("Hotel.jsx Hotel: ", hotel);
+    console.log("Your Bookings: ", currentUser.bookings);
     // Add hotel to User's Bookings
   }
 
   // Create a function to determine if the hotel is booked with the current user
   return(
     <div id='hotels'>
-        {/* Display if currentUser has already booked a room in that Hotel... */}
         <p>{hotel.name}</p><p>{hotel.city}, {hotel.country}</p>
         {/* Toggling the Booking button toggles whether currentUser has booked in that Hotel */}
         <button onClick={toggleBooking}>{booked ? ("Booked") : ("Book Now")}</button>
         {/* Allow Hotels to be globally deleted */}
-        {/* Test if Button and function work */}
         <button onClick={deleteHotel}>Delete</button>
     </div>
-  )
+  );
 }
 
 export default Hotel
