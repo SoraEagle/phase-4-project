74a653b6045f1cb67162247bb98be146be851d98
Matthew Phillips <phillips.matthew829@gmail.com>
2022-10-04 15:04:48 -0500

    Remove unneeded code

--- client/src/components/Hotels/Bookings.jsx
+++ client/src/components/Hotels/Bookings.jsx
@@ -4,9 +4,10 @@ import {BookingsContext} from '../../context/bookingsList';
 
 function Bookings({currentUser, booked, setBooked}){
   const {bookings, setBookings} = useContext(BookingsContext); //Use to filter from the current user's bookings ONLY; Should narrow the scope of the search
+  console.log("Bookings.jsx Bookings: ", bookings);
   return(
     <div><h2>My Bookings</h2>
-      {bookings.map((booking) => {return <h5 key={booking.id}><Booking booking={booking} currentUser={currentUser} booked={booked} setBooked={setBooked} /></h5>})}
+      {bookings.map((booking) => {return <h5 key={booking.id}><Booking booking={booking} currentUser={currentUser} /></h5>})}
     </div>
   );
 }
--- client/src/components/Hotels/EditHotel.jsx
+++ client/src/components/Hotels/EditHotel.jsx
@@ -3,10 +3,7 @@ import {headers} from '../../../src/Globals';
 
 function EditHotel({hotel, onUpdateHotel}){
     const [hotelName, setHotelName] = useState("");
-    // const [hotelCity, setHotelCity] = useState(hotel.city);
-    // const [hotelCountry, setHotelCountry] = useState(hotel.country);
 
-    // use a useEffect to set hotelName = hotel.name
     useEffect(() => {
       hotel.name = hotelName; // Sets the Hotel object's name to the state of hotelName
       console.log("hotelName: ", hotelName);
@@ -15,14 +12,10 @@ function EditHotel({hotel, onUpdateHotel}){
   
     function handleFormSubmit(e){
       e.preventDefault();
-      // console.log("hotel.id: ", hotel.id);
       console.log("hotel: ", hotel);
       console.log("hotelName: ", hotelName);
       console.log("e.target.name.value: ", e.target.name.value);
       console.log("hotel.name: ", hotel.name);
-      // console.log("hotel.city: ", hotel.city);
-      // console.log("hotel.country: ", hotel.country);
-      debugger
 
       const updateHotel = {
         name: (hotelName)
@@ -34,35 +27,26 @@ function EditHotel({hotel, onUpdateHotel}){
       method: "PATCH",
       headers: headers,
       body: JSON.stringify({
-        name: hotelName,
-        // city: hotelCity,
-        // country: hotelCountry
+        name: hotelName
       }),
     })
       .then((r) => r.json())
       .then((data) => {
-        // debugger
         console.log("data: ", data);
         onUpdateHotel(data);
       });
     }
 
     const handleChange = (e) => {
-      // debugger
       setHotelName(e.target.value);
       hotel.name = hotelName; // Sets the Hotel object's name to the state of hotelName
       console.log("e.target.value: ", e.target.value);
-      // console.log(hotelName);
     }
   
     return(
         <form onSubmit={handleFormSubmit}>
             <input type="text" name="name"
             autoComplete='off' value={hotelName} onChange={handleChange}/>
-            {/* <input type="text" name="city"
-            autoComplete='off' value={hotelCity} onChange={(e) => setHotelCity(e.target.city.value)} />
-            <input type="text" name="country"
-            autoComplete='off' value={hotelCountry} onChange={(e) => setHotelCountry(e.target.country.value)} /> */}
             <input type="submit" value="Save" />
         </form>
     );
