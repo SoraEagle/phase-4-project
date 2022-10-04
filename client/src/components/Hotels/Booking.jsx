df5fefa4cd851c54a98e683fe529eb59979e8a27
Matthew Phillips <phillips.matthew829@gmail.com>
2022-10-04 15:10:24 -0500

    Remove uneeded code

--- client/src/components/Hotels/Booking.jsx
+++ client/src/components/Hotels/Booking.jsx
@@ -1,18 +1,16 @@
 import React, {useContext} from 'react';
 import {BookingsContext} from '../../context/bookingsList';
 
-function Booking({booking, currentUser, booked, setBooked}){
+function Booking({booking, currentUser}){
   const {bookings, setBookings} = useContext(BookingsContext);
 
   function deleteBookings(){
-    fetch(`http://localhost:3001/users/${currentUser.id}/bookings/${booking.id}`, {
+    fetch(`/users/${currentUser.id}/bookings/${booking.id}`, {
       method: "DELETE"
     })
     .then((r) => {
       if(r.ok){
-        onDeleteBookings(booking.id);
-        console.log("bookingId: ", booking.id);
-        setBooked(false);
+        onDeleteBookings(booking);
       }
     })
   }
@@ -20,13 +18,12 @@ function Booking({booking, currentUser, booked, setBooked}){
   function onDeleteBookings(deletedBooking){
     const updatedBookings = bookings.filter((booking) => booking.id !== deletedBooking.id);
     setBookings(updatedBookings);
-    console.log("New Bookings: ", bookings);
   }
 
   return(
     <div id='bookings'>
       <p>{booking.hotel.name}</p><p>{booking.hotel.city}</p>
-      <button id='bookings_button' key={booking.id} onClick={deleteBookings}>Cancel Booking</button>
+      <button id='bookings_button' onClick={deleteBookings}>Cancel Booking</button>
       </div>
   );
 }
