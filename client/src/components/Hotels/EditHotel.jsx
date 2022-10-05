18ce7e80c6c4abb07e67352fbdd66ae1d974984b
Matthew Phillips <phillips.matthew829@gmail.com>
2022-10-05 13:41:24 -0500

    Refactor update mechanism

--- client/src/components/Hotels/EditHotel.jsx
+++ client/src/components/Hotels/EditHotel.jsx
@@ -5,23 +5,13 @@ function EditHotel({hotel, onUpdateHotel}){
     const [hotelName, setHotelName] = useState("");
 
     useEffect(() => {
-      hotel.name = hotelName; // Sets the Hotel object's name to the state of hotelName
-      console.log("hotelName: ", hotelName);
-      console.log("hotel.name: ", hotel.name); // The current hotel.name for the Hotel object
-    }, [hotelName]);
+      setHotelName(hotel.name);
+    }, []);
   
     function handleFormSubmit(e){
       e.preventDefault();
-      console.log("hotel: ", hotel);
-      console.log("hotelName: ", hotelName);
-      console.log("e.target.name.value: ", e.target.name.value);
-      console.log("hotel.name: ", hotel.name);
 
-      const updateHotel = {
-        name: (hotelName)
-      }
-
-      console.log("updateHotel: ", updateHotel);
+      const updateHotel = {name: (hotelName)}
   
       fetch(`/hotels/${hotel.id}`, { // PATCH fetch request  is not currently working (404 Not Found)
       method: "PATCH",
@@ -32,16 +22,11 @@ function EditHotel({hotel, onUpdateHotel}){
     })
       .then((r) => r.json())
       .then((data) => {
-        console.log("data: ", data);
         onUpdateHotel(data);
       });
     }
 
-    const handleChange = (e) => {
-      setHotelName(e.target.value);
-      hotel.name = hotelName; // Sets the Hotel object's name to the state of hotelName
-      console.log("e.target.value: ", e.target.value);
-    }
+    const handleChange = (e) => { setHotelName(e.target.value); }
   
     return(
         <form onSubmit={handleFormSubmit}>
