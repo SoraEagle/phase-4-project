ba135e776c89c1c60989746db61716890d4312dd
Matthew Phillips <phillips.matthew829@gmail.com>
2022-10-06 14:49:37 -0500

    Add date to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_191601) do
+ActiveRecord::Schema.define(version: 2022_10_06_175958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_07_15_191601) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date"
   end
 
   create_table "hotels", force: :cascade do |t|
