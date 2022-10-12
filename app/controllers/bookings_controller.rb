c08f5b8040e015fe3b0f70cdb5f35caa13270e6c
Matthew Phillips <phillips.matthew829@gmail.com>
2022-10-12 14:07:16 -0500

    Fix and remove some Bookings methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,8 +2,6 @@ class BookingsController < ApplicationController
     skip_before_action :authorize, only: [:create, :destroy]
     def index
         if params[:user_id]
-            # Need to change to session hash
-            # user = User.find(params[:user_id])
             bookings = set_user.bookings
         else
             bookings = Booking.all
@@ -12,7 +10,6 @@ class BookingsController < ApplicationController
     end
 
     def create
-        # I should be using the user in the session hash
         booking = set_user.bookings.new(booking_params)
         if booking.save
             render json: booking, status: :created
@@ -22,19 +19,16 @@ class BookingsController < ApplicationController
     end
 
     def destroy
-        # Find the user
-        # Instead of hitting model, user.bookings
         @booking = set_user.bookings.find(params[:id])
         @booking.destroy
     end
 
     private
-    def set_user # Method that finds the user in the session hash
+    def set_user
         return user = User.find(session[:user_id])
     end
 
-    def booking_params # Get rid of user_id
+    def booking_params
         params.require(:booking).permit(:id, :hotel_id, :date)
     end
 end
-# Make sure everything works properly!
